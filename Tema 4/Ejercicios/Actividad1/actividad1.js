cc159eb17f5efa2076efa940d3cf92709e4db053
Denthor99 <darancuga@hotmail.com>
2023-09-28 10:37:42 +0200

    Inicio Tema 4 y planteamiento actividades - 28/09/2023

---
+++ Tema 4/Ejercicios/Actividad1/actividad1.js
@@ -0,0 +1,17 @@
+// Función para menu, función para recoger la figura, función para recoger las variables, función para las variables, función para salir
+function menu() {
+  var opciones = Number(
+    prompt(
+      "Elige una opción:\n1. Elija entre cubo, esfera o cilindro.\n2. Introduzca los valores de las variables.\n3.Cálculo de volumen.\n4. Cálculo de área. \n5. Finalizar."
+    )
+  );
+    alert(opciones);
+  switch (opciones) {
+    case 1:
+        
+      break;
+    case 2;
+    default:
+      break;
+  }
+}
