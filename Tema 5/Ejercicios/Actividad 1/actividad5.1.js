5660c5dd799e1249a3bc1b516ce94eccd058897b
Den <drodsan2708@g.educaand.es>
2023-10-11 01:45:29 +0200

    Rango terminado y Moda medio planteado

--- Tema 5/Ejercicios/Actividad 1/actividad5.1.js
+++ Tema 5/Ejercicios/Actividad 1/actividad5.1.js
@@ -7,6 +7,47 @@
  * 
  */
 
+function meterValores(){
+    var numArrray = []; // Declaramos array vacío
+    var numIntroducido = 0;
+    var i = 0;
+    do{ // El bucle se ejecutará hasta que el valor introducido sea no númerico. Se deberán controlar otras cosas dentro de la función
+        numIntroducido = Number(prompt("Introduce el valor " + i + ": \nSi desea dejar de introducir valores escriba cualquier cáracter o cadena"));
+        if (isNaN(numIntroducido) || (numIntroducido==null) || (numIntroducido=="")){
+            break;
+        }
+        numArrray[i] = numIntroducido;
+        i++;
+    }while(true)
+    return numArrray;
+}
+
+function mediaArreglo(valores) {
+    var arrayAnterior = valores;
+    var suma = 0;
+    var media = 0;
+    for (var i = 0; i < arrayAnterior.length; i++) {
+        suma = suma + valores[i];
+    } 
+    media = suma / arrayAnterior.length;
+    alert("La media del Array es " + media);
+}
+
+function rangoArreglo(valores){
+    var arrayAnterior = valores;
+    var minValor = arrayAnterior[0]; // Hacemos referencia al primer valor del array, para poder realizar las comparaciones
+    var maxValor = arrayAnterior[0];
+    for(var i=1;i<arrayAnterior.length;i++){
+        if (arrayAnterior[i] > maxValor){
+            maxValor = arrayAnterior[i];
+        }
+        if (arrayAnterior[i] < minValor){
+            minValor = arrayAnterior[i];
+        }
+    }
+    var rango = maxValor - minValor;
+    alert("El rango del array es de "+ rango);
+}
 
 /**
  * Modas
@@ -22,54 +63,51 @@
  * if(..)
  *  x[s[a]]++;
  * }
- * 
 */
-function meterValores(){
-    var numArrray = []; // Declaramos array vacío
-    var numIntroducido = 0;
-    var i = 0;
-    do{ // El bucle se ejecutará hasta que el valor introducido sea no númerico. Se deberán controlar otras cosas dentro de la función
-        numIntroducido = Number(prompt("Introduce el valor " + i + ": \nSi desea dejar de introducir valores escriba cualquier cáracter o cadena"));
-        if (isNaN(numIntroducido)==false){
-            numArrray[i]=0;
-            break;
-        }
-        numArrray[i] = numIntroducido;
-        i++;
-    }while(!isNaN(numIntroducido))
-    return numArrray;
-}
 
-function mediaArreglo(valores) {
+function modaArreglo(valores){
     var arrayAnterior = valores;
-    var suma = 0;
-    var media = 0;
-    for (var i = 0; i < arrayAnterior.length-1; i++) {
-        suma = suma + valores[i];
-    } 
-    media = suma / arrayAnterior.length;
-    alert("La media del Array es " + media);
-}
-
-function rangoArreglo(valores){
-    var arrayAnterior = valores;
-    var arrayOrdenado = [];
-    for(var i=1;i<arrayAnterior.length;i++){
-        if (arrayAnterior[i-1]>arrayAnterior[i]){
-            arrayOrdenado[i];
+    var moda = 0;
+    var conjuntoModas = {}
+    var contador = {}; //Contamos cuantas veces se repiten los valores introducidos 
+    var maxRepeticion = 0;
+    for (var i = 0; i < arrayAnterior.length; i++) {
+        var num = arrayAnterior[i];
+        if(contador[num]){
+            contador[num]++;
+        } else {
+            contador[num] = 1;
         }
+
+        // Comprobamos la frecuencia de los numeros
+        if (contador[num] > maxRepeticion){
+            maxRepeticion = contador[num];
+            conjuntoModas = [num];
+        } else if (contador[num] === maxRepeticion){
+            conjuntoModas.push(num);
+        }
+        
     }
+    if (maxRepeticion === 1){
+        alert("No hay moda en el arreglo");
+    } else if (conjuntoModas.length == 1){
+        alert("La moda del arreglo es: " + conjuntoModas[0]);
+    } else if(conjuntoModas.length > 1){
+        alert("Las modas de la serie son: " + conjuntoModas);
+    } else{ 
+        alert("No hay modas en el arreglo");
+    }
+
 }
 
-function modaArreglo(){
-
-}
-
+// Esta funciónn será la encargada de ejecutar el programa
 function operaciones(){
     var valores = 0;
     if (valores == 0){
         valores=meterValores();
     } 
         mediaArreglo(valores);
+        rangoArreglo(valores);
+        modaArreglo(valores);
     
 }
