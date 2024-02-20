6475d2ef2bfe89e5f4c233e3e78f0aa342c7aa15
Den <drodsan2708@g.educaand.es>
2024-02-21 00:27:04 +0100

    Actividad 14.2 Terminada

--- Tema 14/actividades/tarea14.2/mongodb.js
+++ Tema 14/actividades/tarea14.2/mongodb.js
@@ -1,10 +1,14 @@
+// Importamos el módulo de MongoDB
 import { MongoClient } from "mongodb";
-// Datos de conexión
 
-const uri =
-  "mongodb+srv://Denthor99:5BbjUL2Vjwpn8uGb@atlascluster.5rucaax.mongodb.net/?retryWrites=true&w=majority";
+// Datos de la conexión a MongoDB
+const uri ="mongodb+srv://Denthor99:5BbjUL2Vjwpn8uGb@atlascluster.5rucaax.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
+/**
+ * conectLectura(): 
+ * Devuelve un array con todos los datos de la colección
+ */
 export async function conectLectura() {
   try {
     await client.connect();
@@ -18,11 +22,14 @@ export async function conectLectura() {
   }
 }
 
+/**
+ * conectEscritura(req): 
+ * Inserta un nuevo documento en la colección
+ */
 export async function conectEscritura(req) {
     try {
         await client.connect();
         const coleccion = client.db("instituto").collection("personas");
-        console.log(req);
         const objeto = req;
         const result = await coleccion.insertOne(objeto);
     } catch (error) {
