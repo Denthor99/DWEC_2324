07f0dff07572ed9757cb02b4fbfded50d9a9ebeb
Denthor99 <darancuga@hotmail.com>
2024-02-29 10:37:43 +0100

    Inicio actividaad 15.3

---
+++ Tema 15/actividades/holaMundoRoute/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MenuComponent } from './menu/menu.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatLabel } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule, MatCardSubtitle, MatCardTitle,MatCardHeader} from '@angular/material/card';
+import { MatGridTile } from '@angular/material/grid-list';
+import { MatCardContent } from '@angular/material/card';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ProductosComponent,
+    ContactoComponent,
+    PagenotfoundComponent,
+    ChildAComponent,
+    ChildBComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatGridTile,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
